ea637a1320c472ff409597565f1390bc7a83f5db
LulaSzDani <lulafelipe7@gmail.com>
2024-08-12 19:57:27 -0300

    quarto ex

---
+++ Quarto.php
@@ -0,0 +1,18 @@
+<?php 
+	$data = "12 de agosto de 2024";
+	$salario = 850.00;
+	$idade = 18;
+	$cargo = "estagiario";
+	$resultado = true;
+
+
+	
+	echo "data: $data <br />";
+echo "salario $salario <br />";
+echo "idade: $idade <br />";
+echo "cargo: $cargo <br />";
+
+	if ($resultado) {
+		echo "resultado é verdadeiro.";
+	}
+ ?>
